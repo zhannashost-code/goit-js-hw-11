a9b965df8905438492361592f85089a196977de9
zhannashost-code <zhannashost@gmail.com>
2026-05-01 10:44:24 +0000

    Deploying to gh-pages from @ zhannashost-code/goit-js-hw-11@64c137f8138793131696163b2d51a5501c6e5c9d 🚀

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import{a as l,S as u,i as n}from"./assets/vendor-D1AWmRWP.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const f="55660188-792f1fef602df37d72b3dd925";function d(s){return l.get("https://pixabay.com/api/",{params:{key:f,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(t=>t.data)}const c=document.querySelector(".gallery"),m=new u(".gallery a");function y(s){const t=s.map(o=>`
+import{a as l,S as u,i as n}from"./assets/vendor-D1AWmRWP.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const f="55660188-792f1fef602df37d72b3dd925";function d(s){return l.get("https://pixabay.com/api/",{params:{key:f,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(o=>o.data)}const c=document.querySelector(".gallery"),m=new u(".gallery a");function p(s){const o=s.map(r=>`
     <li class="gallery-item">
-      <a href="${o.largeImageURL}">
-        <img src="${o.webformatURL}" alt="${o.tags}" />
+      <a href="${r.largeImageURL}">
+        <img src="${r.webformatURL}" alt="${r.tags}" />
       </a>
-      <p>❤️ ${o.likes}</p>
+      
+      <div class="info">
+        <p><b>Likes</b> ${r.likes}</p>
+        <p><b>Views</b> ${r.views}</p>
+        <p><b>Comments</b> ${r.comments}</p>
+        <p><b>Downloads</b> ${r.downloads}</p>
+      </div>
     </li>
-  `).join("");c.insertAdjacentHTML("beforeend",t),m.refresh()}function h(){c.innerHTML=""}function p(){document.querySelector(".loader").classList.add("visible")}function g(){document.querySelector(".loader").classList.remove("visible")}const L=document.querySelector(".form");L.addEventListener("submit",b);function b(s){s.preventDefault();const t=s.target.elements["search-text"].value.trim();if(!t){n.error({message:"Введи щось для пошуку!"});return}h(),p(),d(t).then(o=>{if(o.hits.length===0){n.error({message:"Sorry, there are no images matching your search query."});return}y(o.hits)}).catch(()=>{n.error({message:"Помилка запиту 😢"})}).finally(()=>{g()})}
+  `).join("");c.insertAdjacentHTML("beforeend",o),m.refresh()}function y(){c.innerHTML=""}function h(){document.querySelector(".loader").classList.add("visible")}function g(){document.querySelector(".loader").classList.remove("visible")}const b=document.querySelector(".form");b.addEventListener("submit",L);function L(s){s.preventDefault();const o=s.target.elements["search-text"].value.trim();if(!o){n.error({message:"Введи щось для пошуку!"});return}y(),h(),d(o).then(r=>{if(r.hits.length===0){n.error({message:"Sorry, there are no images matching your search query."});return}p(r.hits)}).catch(()=>{n.error({message:"Помилка запиту 😢"})}).finally(()=>{g()})}
 //# sourceMappingURL=index.js.map
